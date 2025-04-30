0ee350faa072c983515e0666d8a38c2f38ba9805
Neurolancer81 <87434607+Neurolancer81@users.noreply.github.com>
2025-04-30 16:11:32 -0400

    Attribute set added

--- Source/TBS_Fantasy/Private/AbilitySystem/TBSAttributeSet.cpp
+++ Source/TBS_Fantasy/Private/AbilitySystem/TBSAttributeSet.cpp
@@ -3,3 +3,71 @@
 
 #include "AbilitySystem/TBSAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UTBSAttributeSet::UTBSAttributeSet()
+{
+	InitHealth(100.0f);
+	InitMaxHealth(100.0f);
+	InitStrength(10.f);
+	InitDexterity(10.f);
+	InitConstitution(10.f);
+	InitIntelligence(10.f);
+	InitWisdom(10.f);
+	InitCharisma(10.f);
+}
+
+void UTBSAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Strength, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Dexterity, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Constitution, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Intelligence, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Wisdom, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UTBSAttributeSet, Charisma, COND_None, REPNOTIFY_Always);
+}
+
+void UTBSAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Health, OldHealth);
+	
+}
+
+void UTBSAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UTBSAttributeSet::OnRep_Strength(const FGameplayAttributeData& OldStrength) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Strength, OldStrength);
+}
+
+void UTBSAttributeSet::OnRep_Dexterity(const FGameplayAttributeData& OldDexterity) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Dexterity, OldDexterity);
+}
+
+void UTBSAttributeSet::OnRep_Constitution(const FGameplayAttributeData& OldConstitution) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Constitution, OldConstitution);
+}
+
+void UTBSAttributeSet::OnRep_Intelligence(const FGameplayAttributeData& OldIntelligence) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Intelligence, OldIntelligence);
+}
+
+void UTBSAttributeSet::OnRep_Wisdom(const FGameplayAttributeData& OldWisdom) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Wisdom, OldWisdom);
+}
+
+void UTBSAttributeSet::OnRep_Charisma(const FGameplayAttributeData& OldCharisma) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UTBSAttributeSet, Charisma, OldCharisma);
+}
--- Source/TBS_Fantasy/Public/AbilitySystem/TBSAttributeSet.h
+++ Source/TBS_Fantasy/Public/AbilitySystem/TBSAttributeSet.h
@@ -4,9 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "TBSAttributeSet.generated.h"
 
-
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -14,5 +19,67 @@ UCLASS()
 class TBS_FANTASY_API UTBSAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UTBSAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/***************** ATTRIBUTE DEFINITIONS ***************************/
+
+	// Health
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Health)
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	// MaxHealth
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, MaxHealth)
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	// Strength
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Strength, Category="Attributes")
+	FGameplayAttributeData Strength;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Strength)
+	UFUNCTION()
+	void OnRep_Strength(const FGameplayAttributeData& OldStrength) const;
+
+	// Dexterity
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Dexterity, Category="Attributes")
+	FGameplayAttributeData Dexterity;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Dexterity)
+	UFUNCTION()
+	void OnRep_Dexterity(const FGameplayAttributeData& OldDexterity) const;
+
+	// Constitution
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Constitution, Category="Attributes")
+	FGameplayAttributeData Constitution;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Constitution)
+	UFUNCTION()
+	void OnRep_Constitution(const FGameplayAttributeData& OldConstitution) const;
+
+	// Intelligence
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Intelligence, Category="Attributes")
+	FGameplayAttributeData Intelligence;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Intelligence)
+	UFUNCTION()
+	void OnRep_Intelligence(const FGameplayAttributeData& OldIntelligence) const;
+
+	// Wisdom
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Wisdom, Category="Attributes")
+	FGameplayAttributeData Wisdom;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Wisdom)
+	UFUNCTION()
+	void OnRep_Wisdom(const FGameplayAttributeData& OldWisdom) const;
+
+	// Charisma
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Charisma, Category="Attributes")
+	FGameplayAttributeData Charisma;
+	ATTRIBUTE_ACCESSORS(UTBSAttributeSet, Charisma)
+	UFUNCTION()
+	void OnRep_Charisma(const FGameplayAttributeData& OldCharisma) const;
+
 };
